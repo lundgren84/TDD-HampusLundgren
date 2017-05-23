75b31b9d1e55f225de10ef2339e953894b36eafd
Hampus Lundgren <Lundgren84@hotmail.se>
2017-05-23 22:18:56 +0200

    CoinChangerDone

--- TDD-HampusLundgren/CoinChangerApplication.Tests/CoinChangerTests.cs
+++ TDD-HampusLundgren/CoinChangerApplication.Tests/CoinChangerTests.cs
@@ -44,6 +44,20 @@ namespace CoinChangerApplication.Tests
             Assert.AreEqual(1, myChange[5m]);
             Assert.AreEqual(5, myChange[10m]);
         }
+        [Test]
+        public void ChangeWithNotWholeNumbers()
+        {
+            var coinTypes = new List<decimal> { 0.25m, 0.50m, 1.0m, 5.0m };
+            var sut = new CoinChanger(coinTypes);
+
+            Dictionary<decimal, int> myChange = sut.MakeChange(13.75m);
+
+            Assert.AreEqual(1, myChange[0.25m]);
+            Assert.AreEqual(1, myChange[0.5m]);
+            Assert.AreEqual(3, myChange[1m]);
+            Assert.AreEqual(2, myChange[5m]);
+           
+        }
     }
 
  
